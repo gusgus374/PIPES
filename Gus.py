f15568a4de93adae4696e8ee114d57fee750b86d
gusgus374 <gusalvarez374@gmail.com>
2024-06-07 11:04:13 -0400

    upated intro

--- Gus.py
+++ Gus.py
@@ -1,12 +1,98 @@
 import streamlit as st
 import pandas as pd
 import altair as alt
+import streamlit.components.v1 as components
 
-st.title("This is my title")
+st.set_page_config(
+    page_title="FootyLab",
+    page_icon="./resources/boots1.png",
+    layout="wide",
+    initial_sidebar_state="expanded",
+    menu_items={
+        'Get Help': 'https://datarook.com/',
+        'Report a bug': "https://datarook.com/#copyright",
+        'About': "# This is a version of FootyLab created for the 2024 UTK PIPES Investigators Camp. Contact gus@datarook.com to learn more"
+    }
+)
+with st.echo(code_location="below"):
+    import streamlit as st
+    import pandas as pd
+    import altair as alt
+    import streamlit.components.v1 as components
+    st.title("Welcome to our 2024 UTK PIPES Soccer Data Science App!")
 
-st.write("Data... stores information")
-st.write("Data is the information we gathered")
-st.header("Data is a Collection of Structured Information!")
+    st.header("Soccer... and Data... *and* Science?")
+    st.subheader("One of the biggest soccer clubs in the world, Liverpool FC, hired particle physicists to help improve their soccer team")
+
+    col1, col2 = st.columns(2)
+    col1.write("They used their knowledge of this")
+    col2.write("And combined it with soccer data to create this (known as the Pitch Control model):")
+    with col2:
+        iframe_src2 = "https://www.youtube.com/embed/Nc3uFWnPlsQ?si=pUx4ouf0EhWYMrVE"
+        components.iframe(iframe_src2,600,500)
+
+    with col1:
+        iframe_src = "https://phet.colorado.edu/sims/html/charges-and-fields/latest/charges-and-fields_en.html"
+        components.iframe(iframe_src,height=500)
+    st.subheader("Soccer and Science.")
+    st.header("What about that data thing? What *is* data?")
+    st.write("Well first we collect information like: 'What is your hobby? 'Where do you go to school?' 'What is your favorite sport?' 'What do you want to study?'")
+    st.write("and then we organize it!")
+
+    info = {'Name': ['Zeb','Mirie','Dani'], 'Hobby':['Motorcycle Racing','Drawing','Video Games'], 'Educational Interest': ['Engineering/physics','Kinesiology','Veterinary Science'], 'Favorite Sport': ['Baseball', 'Soccer', 'Baseball'], 'Grade Level': ['Senior','Senior','Junior'], 'Current School':['Sequoyah HS','Sweetwater HS', 'Sequoya HS']}
+    campers_db = pd.DataFrame(data=info)
+    st.dataframe(campers_db)
+
+    st.header("So, Data is a *Collection* of *Structured* *Information*")
+
+    st.header("Okay, so I know what Soccer is but what do we mean by ''Data Science''?")
+
+    st.image("./resources/datascience.png")
+
+    st.header("Hacking Skills = ~Computer Programming~ *Magic*")
+    iframe_src2 = "https://www.youtube.com/embed/Qgr4dcsY-60?si=gsK8I_rpz0cpH5UO"
+    components.iframe(iframe_src2,400,300)
+    st.header("We will now explore some soccer data with...")
+    st.subheader("Magic.")
+    st.markdown("Well... actually just writing some *python code*... which feels like magic, I promise.")
+
+    st.markdown("In fact, everything you are currently seeing on the screen was written using python code!!!. Have a look for youself:")
+
+st.subheader("See? Magic.")
+coach_message = st.chat_message(name="Coach Gus",avatar="./resources/profile_coachGus.JPG")
+coach_message.write("Now I'm going to ``cast a spell`` (:wink:) to generate a button:")
+
+st.code("""
+        #this spell is actually just python code
+st.button("I'm a Button")
+        """)
+st.button("I'm a Button")
+
+coach_message = st.chat_message(name="Coach Gus",avatar="./resources/profile_coachGus.JPG")
+coach_message.write("Okay cool! We can click on our newly casted button but... that's about it. Let's try a slightly more advanced spell:")
+
+st.code("""
+if st.button("Click me for a celebration"):
+        st.balloons()
+        """)
+
+if st.button("Click me for a celebration"):
+        st.balloons()
+
+
+st.divider()
+list = [27,68,71,87,91,100]
+st.write(len(list))
+my_sum = 0
+for number in list:
+    my_sum = my_sum + number
+
+average = my_sum/len(list)
+st.write(my_sum)
+st.write(average)
+st.write(27 + 68 + 71 + 87 + 91 + 100)
+st.write(27*6)
+
 if st.button("CELEBRATE"):
     st.balloons()
 
@@ -33,21 +119,29 @@ if uploaded_file is not None:
 
     #st.scatter_chart(animal_db,x="Name",y="Speed (km/H)")
 
+    db["goals per shot"] = db["Goals"]/db["Total Shots (inc. Blocks)"]
+
     variable_x = st.selectbox("Pick Your X Variable!",db.columns.to_list(),1)
     variable_y = st.selectbox("Pick Your Y Variable!",db.columns.to_list(),0)
     
 
     st.scatter_chart(db,x=variable_x,y=variable_y)
 
+    
+
     variable_size = st.selectbox("What determines the size of the dots?",db.columns.to_list(),3)
-    variable_color = st.selectbox("What determines the color of the data points?",db.columns.to_list(),4)
+    variable_color = st.selectbox("What determines the color of the data points?",db.columns.to_list(),2)
+    
+
+    
+    #st.write(goalspershot)
 
     chart = alt.Chart(db).mark_circle().encode(
             x=variable_x,
             y=variable_y,
             size=alt.Size(variable_size,legend=None),
             color=alt.Color(variable_color,legend=None),
-            tooltip=["Player","Team","Age"]).properties(height=500).interactive()
+            tooltip=["Player","Team","Age","Goals","Total Shots (inc. Blocks)"]).properties(height=500).interactive()
     
     st.altair_chart(chart, theme="streamlit", use_container_width=True)
 
